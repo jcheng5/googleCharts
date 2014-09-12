03d2dc6e67d35af2775d52c88ab6921517829f54
Joe Cheng <joe@rstudio.org>
2014-09-12 13:49:54 -0700

    Provide explicit colors for the regions

--- inst/examples/bubble/global.R
+++ inst/examples/bubble/global.R
@@ -1,1 +1,2 @@
 data <- readRDS("healthexp.Rds")
+data$Region <- as.factor(data$Region)
--- inst/examples/bubble/server.R
+++ inst/examples/bubble/server.R
@@ -1,12 +1,22 @@
 library(dplyr)
 
 shinyServer(function(input, output, session) {
+
+  # Provide explicit colors for regions, so they don't get recoded when the
+  # different series happen to be ordered differently from year to year.
+  # http://andrewgelman.com/2014/09/11/mysterious-shiny-things/
+  defaultColors <- c("#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477")
+  series <- structure(
+    lapply(defaultColors, function(color) { list(color=color) }),
+    names = levels(data$Region)
+  )
+
   yearData <- reactive({
     # Filter to the desired year, and put the columns
     # in the order that Google's Bubble Chart expects
     # them (name, x, y, color, size). Also sort by region
-    # so that Google Charts colors the regions consistently
-    # (thanks Prof. Gelman).
+    # so that Google Charts orders and colors the regions
+    # consistently.
     df <- data %.%
       filter(Year == input$year) %.%
       select(Country, Health.Expenditure, Life.Expectancy,
@@ -21,7 +31,8 @@ shinyServer(function(input, output, session) {
       options = list(
         title = sprintf(
           "Health expenditure vs. life expectancy, %s",
-          input$year)
+          input$year),
+        series = series
       )
     )
   })
